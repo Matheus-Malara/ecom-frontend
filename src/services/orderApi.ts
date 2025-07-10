2f2199d0acc0e842317c23389dc8aa374a844854
Matheus-Malara <matheus_malara@live.com>
2025-07-09 21:46:04 -0300

    Add order history functionality to account page with tabbed navigation and updated UI

--- src/services/orderApi.ts
+++ src/services/orderApi.ts
@@ -1,6 +1,7 @@
 import type {Order} from "@/types/order";
 import type {StandardResponse} from "@/types/api-response.ts";
 import api from "@/services/axiosInstance.ts";
+import type {Page} from "@/types/paginated.ts";
 
 export async function createOrder(): Promise<Order> {
     const response = await api.post<StandardResponse<Order>>("/orders/checkout");
@@ -10,4 +11,9 @@ export async function createOrder(): Promise<Order> {
 export async function getOrderById(id: number): Promise<Order> {
     const response = await api.get<StandardResponse<Order>>(`/orders/${id}`);
     return response.data.data;
+}
+
+export async function getUserOrders(page: number = 0, size: number = 10): Promise<StandardResponse<Page<Order>>> {
+    const response = await api.get<StandardResponse<Page<Order>>>(`/orders?page=${page}&size=${size}`);
+    return response.data;
 }
